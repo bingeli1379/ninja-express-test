e58f0f9079fb82dfbc1893cbf5d180361ef06df4
Eli <bingeli1379@gmail.com>
2022-07-14 18:02:35 +0800

    add middleware and route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
-
 const app = express()
+
+app.use((req, res, next) => {
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'message' })
+})
 
 app.listen(process.env.PORT)
