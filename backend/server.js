499791e4e5d1a90246582b8f6b6cc3b475594196
Eli <bingeli1379@gmail.com>
2022-07-17 20:52:13 +0800

    add mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
-
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts.js')
+
+const app = express()
 
 app.use(express.json())
 app.use((req, res, next) => {
@@ -12,4 +13,12 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT)
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(process.env.PORT)
+    console.log(`listen on port: ${process.env.PORT}`)
+  })
+  .catch(err => {
+    console.error(err)
+  })
